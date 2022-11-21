7475727258dbb6d17ea45dbce35691e0cd09465c
gujsingh <gujsingh@ucsc.edu>
2022-11-20 23:52:17 -0800

    project update

--- project/Project.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-# potentially useful function: read_csv
-house <- read_csv(here::here("data/", "house.csv"))
-glimpse(house)
-
-assesements <- read.table("data/school/school1.txt", sep='\t',
-                          header=TRUE)
-glimpse(assesements)
